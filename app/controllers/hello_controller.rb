66cfdcd7b23bff87289b5bde94f469c3776e7c42
Katie <katie.bautista@gmail.com>
2016-04-12 10:09:14 -0400

    first commit

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,14 @@
+class HelloController < ApplicationController
+  def index
+  	@homestate = 'California'
+  	@countries = ["Mexico", "Canada", "Panama"]
+  	@images = ["mexico1.jpg", "canada1.jpg", "panama1.jpg", "panama2.jpg"]
+
+  	@image_hash = ["Mexico City" => "mexico1.jpg", "Canada" => "canada1.jpg", "Panama1" => "panama1.jpg", "Panama2" => "panama2.jpg"]
+  end
+
+  def about
+  	@color = params[:color]
+  	@size = params[:size].to_i
+  end
+end
